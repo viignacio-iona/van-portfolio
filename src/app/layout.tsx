dd1a45fc0bf2c339322ba51b42740360c23318d3
viignacio-iona <van.ignacio@ionacommerce.com>
2025-05-23 21:30:48 +0800

    Enable speed insights in Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import '@fontsource/inter';
 import '@fontsource/montserrat';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
       </head>
       <body className={`${inter.variable} font-sans antialiased bg-white dark:bg-black`}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
