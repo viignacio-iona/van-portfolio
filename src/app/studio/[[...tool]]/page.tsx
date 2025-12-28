6bc2a713d96177160f512811161d78a84e35adb0
viignacio-iona <van.ignacio@ionacommerce.com>
2025-12-28 21:06:01 +0800

    Fix Sanity Studio route: import actual config with plugins and schemas

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,19 +1,14 @@
 'use client'
 
 import { NextStudio } from 'next-sanity/studio'
+import config from '../../../../sanity/sanity.config'
 
 export default function StudioPage() {
   return (
     <NextStudio
       config={{
-        name: 'default',
-        title: 'van-portfolio',
-        projectId: 'givnzkpy',
-        dataset: 'production',
-        plugins: [],
-        schema: {
-          types: []
-        }
+        ...config,
+        basePath: '/studio',
       }}
     />
   )
